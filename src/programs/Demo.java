a49640302cc558cd61a59391b57d1fb5a3863caa
giashuddin <mdgiashu069@gmail.com>
2020-09-26 23:45:02 +0600

    Updated

--- src/programs/Demo.java
+++ src/programs/Demo.java
@@ -4,15 +4,9 @@ import java.util.*;
 
 public class Demo {
 
-    public void doNothing() {
-        System.out.println("Hello");
-    }
-
-
     public static void main(String[] args) {
 
         StudentProcessing studentProcessing = new StudentProcessing();
-
 
         studentProcessing.processEveryThingBeforeExam();
     }
